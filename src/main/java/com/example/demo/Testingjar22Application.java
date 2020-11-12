85cd51d06e4298c8e5e8d6d4129ed55441ebf95b
chaitanya1082001 <74292154+chaitanya1082001@users.noreply.github.com>
2020-11-12 21:07:29 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Testingjar22Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testingjar22Application extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testingjar22Application.class, args);
+	}
+
+}
